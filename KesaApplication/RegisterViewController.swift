a5650948c306a34229d21b902df5575cd8ce2bb1
Debbie Lee <debbielee@Debbies-MacBook-Pro.local>
2016-07-05 20:37:01 -0700

    register page

---
+++ KesaApplication/RegisterViewController.swift
@@ -0,0 +1,108 @@
+//
+//  RegisterViewController.swift
+//  KesaApplication
+//
+//  Created by Joon Lee on 2016-06-20.
+//  Copyright © 2016 kesa. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+  
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setting delegate property to self, so now, RegisterViewController
+        // becomes the delegate of the text fields and is able to implement the
+        // UITextFieldDelegate methods
+        firstName.delegate = self
+        lastName.delegate = self
+        email.delegate = self
+        password.delegate = self
+    }
+    
+    
+    /* When the screen is touched */
+    override func touchesBegan(touches: Set<UITouch>,
+                               withEvent event: UIEvent?) {
+        // For instances where the keyboard is already brought up,
+        // Hide the keyboard
+        self.view.endEditing(true)
+        firstNameLabel.textColor = UIColor.blackColor()
+        lastNameLabel.textColor = UIColor.blackColor()
+        emailLabel.textColor = UIColor.blackColor()
+        passwordLabel.textColor = UIColor.blackColor()
+    }
+    
+    
+    /* Highlighting each labels when their text fields are chosen */
+    func textFieldDidBeginEditing(textField: UITextField) {
+        firstNameLabel.textColor = UIColor.blackColor()
+        lastNameLabel.textColor = UIColor.blackColor()
+        emailLabel.textColor = UIColor.blackColor()
+        passwordLabel.textColor = UIColor.blackColor()
+        
+        switch textField {
+        case firstName:
+            firstNameLabel.textColor = UIColor.orangeColor()
+            
+        case lastName:
+            lastNameLabel.textColor = UIColor.orangeColor()
+            
+        case email:
+            emailLabel.textColor = UIColor.orangeColor()
+            
+        case password:
+            passwordLabel.textColor = UIColor.orangeColor()
+            
+        default:
+            break
+        }
+    }
+    
+    
+    @IBAction func onCreateAccountButtonClickListener(sender: UIButton) {
+        
+        let userFirstName = firstName.text
+        let userLastName = lastName.text
+        let userEmail = email.text
+        let userPassword = password.text
+        
+        // Booleans to check for name/email validity
+        let isFirstNameValid = Utility.isValidNameFormat(userFirstName!)
+        let isLastNameValid = Utility.isValidNameFormat(userLastName!)
+        let isEmailValid = Utility.isValidEmailFormat(userEmail!)
+        
+        if (userFirstName!.isEmpty || !isFirstNameValid) {
+            Utility.displayAlertMessage ("First name field is invalid",
+                                         viewController: self);
+            return
+        } else if (userLastName!.isEmpty || !isLastNameValid) {
+            Utility.displayAlertMessage("Last name field is invalid",
+                                        viewController: self)
+            return
+        } else if (userEmail!.isEmpty || !isEmailValid) {
+            Utility.displayAlertMessage("Email field is invalid",
+                                        viewController: self)
+            return
+        } else if (userPassword!.isEmpty) {
+            Utility.displayAlertMessage("Password field is required",
+                                        viewController: self)
+            return
+        } else {
+            // Store data and etc
+        }
+    }
+}
---
+++ KesaApplication/Utility.swift
@@ -0,0 +1,52 @@
+//
+//  Utility.swift
+//  KesaApplication
+//
+//  Created by Joon Lee on 2016-06-20.
+//  Copyright © 2016 kesa. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+class Utility {
+    
+    class func isValidEmailFormat (email:String) -> Bool {
+        
+        let emailRegEx = "^(?:(?:(?:(?: )*(?:(?:(?:\\t| )*\\r\\n)?(?:\\t| )+))+(?: )*)|(?: )+)?(?:(?:(?:[-A-Za-z0-9!#$%&’*+/=?^_'{|}~]+(?:\\.[-A-Za-z0-9!#$%&’*+/=?^_'{|}~]+)*)|(?:\"(?:(?:(?:(?: )*(?:(?:[!#-Z^-~]|\\[|\\])|(?:\\\\(?:\\t|[ -~]))))+(?: )*)|(?: )+)\"))(?:@)(?:(?:(?:[A-Za-z0-9](?:[-A-Za-z0-9]{0,61}[A-Za-z0-9])?)(?:\\.[A-Za-z0-9](?:[-A-Za-z0-9]{0,61}[A-Za-z0-9])?)*)|(?:\\[(?:(?:(?:(?:(?:[0-9]|(?:[1-9][0-9])|(?:1[0-9][0-9])|(?:2[0-4][0-9])|(?:25[0-5]))\\.){3}(?:[0-9]|(?:[1-9][0-9])|(?:1[0-9][0-9])|(?:2[0-4][0-9])|(?:25[0-5]))))|(?:(?:(?: )*[!-Z^-~])*(?: )*)|(?:[Vv][0-9A-Fa-f]+\\.[-A-Za-z0-9._~!$&'()*+,;=:]+))\\])))(?:(?:(?:(?: )*(?:(?:(?:\\t| )*\\r\\n)?(?:\\t| )+))+(?: )*)|(?: )+)?$"
+        
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        
+        let result = emailTest.evaluateWithObject(email)
+        
+        return result
+    }
+    
+    
+    class func isValidNameFormat (name:String) -> Bool {
+        
+        let decimalCharacters = NSCharacterSet.decimalDigitCharacterSet()
+        let decimalRange =
+            name.rangeOfCharacterFromSet(decimalCharacters,
+                                         options: NSStringCompareOptions(),
+                                         range: nil)
+        
+        return decimalRange == nil
+    }
+    
+    
+    class func displayAlertMessage (userMessage: String, viewController: UIViewController) {
+        
+        let alert = UIAlertController(title: "Alert",
+                                      message: userMessage,
+                                      preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "Ok",
+                                     style: UIAlertActionStyle.Default,
+                                     handler: nil)
+        
+        alert.addAction(okAction)
+        
+        viewController.presentViewController(alert, animated: true, completion: nil)
+    }
+}
